2fd5af81133ae73351605c43ad13f6a7d2f4a84a
Kusti Skytén <kskyten@users.noreply.github.com>
2021-07-02 09:40:43 +0200

    Wrap more diagnostic plots from bayesplot

--- src/bayesplot.jl
+++ src/bayesplot.jl
@@ -18,5 +18,49 @@ end
 function ppc_dens_overlay(y, yrep; kwargs...)
     # TableTraits.isiterabletable(yrep) || error("Argument `yrep` is not an iterable table.")
     @assert length(y) == size(yrep, 2)
-    bayesplot.ppc_deens_overlay(y, DataFrame(yrep); kwargs...)
+    bayesplot.ppc_dens_overlay(y, DataFrame(yrep); kwargs...)
+end
+
+function ppc_rootogram(y, yrep; kwargs...)
+    @assert length(y) == size(yrep, 2)
+    bayesplot.ppc_rootogram(y, DataFrame(yrep); kwargs...)
+end
+
+function ppc_stat(y, yrep; kwargs...)
+    @assert length(y) == size(yrep, 2)
+    bayesplot.ppc_stat(y, DataFrame(yrep); kwargs...)
+end
+
+function ppc_intervals(y, yrep; kwargs...)
+    @assert length(y) == size(yrep, 2)
+    bayesplot.ppc_intervals(y, DataFrame(yrep); kwargs...)
+end
+
+function ppc_stat_grouped(y, yrep; kwargs...)
+    @assert length(y) == size(yrep, 2)
+    bayesplot.ppc_stat_grouped(y, DataFrame(yrep); kwargs...)
+end
+
+function mcmc_hist(x; kwargs...)
+    bayesplot.mcmc_hist(x; kwargs...)
+end
+
+function mcmc_trace(x; kwargs...)
+    bayesplot.mcmc_trace(x; kwargs...)
+end
+
+function mcmc_scatter(x; kwargs...)
+    bayesplot.mcmc_scatter(x; kwargs...)
+end
+
+function mcmc_parcoord(x; kwargs...)
+    bayesplot.mcmc_parcoord(x; kwargs...)
+end
+
+function mcmc_areas(x; kwargs...)
+    bayesplot.mcmc_areas(x; kwargs...)
+end
+
+function bayesplot_grid(plots...; kwargs...)
+    bayesplot.bayesplot_grid(plots...; kwargs...)
 end
