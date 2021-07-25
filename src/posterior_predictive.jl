9d3dfc807c0404ff92208563cb545ce2ab88ce4f
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-07-25 18:19:14 +0100

    added posterior_predictive implementation for Model

--- src/posterior_predictive.jl
+++ src/posterior_predictive.jl
@@ -4,3 +4,8 @@ function posterior_predictive(model, posterior_draws; kwargs...)
     pred = predictive(model, keys(first(posterior_draws))...)(; kwargs...)
     [rand(pred(draw)) for draw in posterior_draws]
 end
+
+function posterior_predictive(model::DynamicPPL.Model, posterior_draws; kwargs...)
+    predictions = predict(model, posterior_draws)
+    return predictions
+end
