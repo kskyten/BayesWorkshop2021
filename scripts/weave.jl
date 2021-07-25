5c3e483c758484d211632aee16fa4038f214e03f
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-07-25 20:42:15 +0100

    improved weave script

--- scripts/weave.jl
+++ scripts/weave.jl
@@ -1,4 +1,5 @@
 using Weave
+using DrWatson
 
 # for f in readdir(joinpath(@__DIR__, "../src/weave/RCall/"), join=true)
 #     weave(f, outpath="../notebooks/RCall")
@@ -10,9 +11,10 @@ using Weave
 
 nbname(f) = splitext(basename(f))[1] * ".ipynb"
 
-for pkg in ["RCall", "Soss"]
-    cd(joinpath(@__DIR__, "../notebooks", pkg)) do
-        for f in readdir(joinpath(@__DIR__, "../src/weave", pkg), join=true)
+for pkg in ["Turing", "Soss", "RCall"]
+    mkpath(projectdir("notebooks", pkg))
+    cd(projectdir("notebooks", pkg)) do
+        for f in readdir(srcdir("weave", pkg), join=true)
             convert_doc(f, nbname(f))
         end
     end
